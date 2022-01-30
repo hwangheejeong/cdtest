93b3fc8f4f9727f483683ed884213f83216c47d8
hwangheejeong <qkscjddl12@gmail.com>
2022-01-30 20:09:23 +0900

    효율적인 화폐단위

--- cdtest/dynamic/effi_money.py
+++ cdtest/dynamic/effi_money.py
@@ -1,7 +1,24 @@
-#효율적인 화폐구성
-#최소한의 화폐개수
+# 효율적인 화폐구성
+# 최소한의 화폐개수
 
-n,m = map(int,input().split())
-money =[]
+# 화폐개수 n개, 각 화폐의 단위 입력
+n, m = map(int, input().split())
+money = []
 for i in range(n):
     money.append(int(input()))
+
+d = [10001] * (m + 1)
+d[0] = 0
+
+for i in money:
+   if i <=m:
+       d[i]=1
+
+for i in money:
+    for j in range(i+1,m+1):
+        d[j]=min(d[j],d[j-i]+1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
